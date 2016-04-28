06e4a48d117c66090892ac309320e0ec61a146db
wangjun <wangjun@edaijia-inc.cn>
2016-04-28 10:31:30 +0800

    通过左侧导航可以切换的原有的页面

--- perf4j-dashboard/src/main/webapp/js/page/nav.js
+++ perf4j-dashboard/src/main/webapp/js/page/nav.js
@@ -21,9 +21,44 @@ $(function () {
         }
     });
 
+    // 获取当前窗口url中param参数的值
+    function get_param(param) {
+        var query = location.search.substring(1).split('&');
+        for (var i = 0; i < query.length; i++) {
+            var kv = query[i].split('=');
+            if (kv[0] == param) {
+                return kv[1];
+            }
+        }
+        return null;
+    }
+
+    // 设置当前窗口url中param的值
+    function set_param(param, value) {
+        var query = location.search.substring(1);
+        var p = new RegExp("(^|&" + param + ")=[^&]*");
+        if (p.test(query)) {
+            query = query.replace(p, "$1=" + value);
+            location.search = '?' + query;
+        } else {
+            if (query == '') {
+                location.search = '?' + param + '=' + value;
+            } else {
+                location.search = '?' + query + '&' + param + '=' + value;
+            }
+        }
+    }
+
     $(document).on('click', '.cluster_nav', function () {
-        var name = $(this).html();
-        window.location.href = "/?method=now&currentCluster=" + name;
+        var url = window.location.href;
+        var currentCluster = $(this).html();
+        var method = get_param("method");
+        if (method == null) {
+            url = "/?method=now&currentCluster=" + currentCluster;
+            window.location.href = url;
+        } else {
+            set_param("currentCluster", currentCluster);
+        }
     });
 
     $(document).on("click", ".top_nav_menu", function () {
